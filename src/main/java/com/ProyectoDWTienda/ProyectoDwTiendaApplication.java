c82dc8af9b7254bbe5bc5e3cfe25fbdd61f82c22
fabricio <furimatarrita15@gmail.com>
2023-03-07 15:46:01 -0600

    Repositorio para Proyecto Final

---
+++ src/main/java/com/ProyectoDWTienda/ProyectoDwTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoDWTienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDwTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDwTiendaApplication.class, args);
+	}
+
+}
